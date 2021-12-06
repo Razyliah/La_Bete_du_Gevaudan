debe25b972107d2aaac3da16123601e204e430ce
Robin <robin2109@gmail.com>
2021-12-06 18:08:10 +0100

    robin commit main

---
+++ Assets/Scripts/UI Fade.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFade : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup myUIGroup;
+
+    public void ShowUI()
+    {
+
+    }
+
+    public void HideUI()
+    {
+
+    }
+
+}
